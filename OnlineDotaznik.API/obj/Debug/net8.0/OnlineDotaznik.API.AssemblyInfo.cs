a6d4a1bd74baf040aa24aed997a063a2dc7051bd
Jan Vokurka <jan.vokurka@arkance-development.com>
2025-04-09 12:11:16 +0200

    env var corrected

--- OnlineDotaznik.API/obj/Debug/net8.0/OnlineDotaznik.API.AssemblyInfo.cs
+++ OnlineDotaznik.API/obj/Debug/net8.0/OnlineDotaznik.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineDotaznik.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d56332a90666e98b32ad4c2d51eb75ddc18e1065")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5377ba167d5fd854830f9bff27f1f099d754499")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineDotaznik.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineDotaznik.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
